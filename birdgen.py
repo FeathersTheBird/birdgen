3fcc11e41646918e52ac71ee8918ee9fb687362b
Hayley S. Morin <hayley.morin@baxter-academy.org>
2017-10-10 11:17:10 -0400

    Add files via upload

---
+++ birdgen.py
@@ -0,0 +1,93 @@
+import random
+
+def main():
+	
+	colors = [
+		"Iridescent",
+		"White",
+		"Pale",
+		"Rainbow",
+		"Red",
+		"Rosy",
+		"Orange",
+		"Sunny",
+		"Yellow",
+		"Golden",
+		"Green",
+		"Verdant",
+		"Blue",
+		"Aqua",
+		"Steel",
+		"Indigo",
+		"Violet"
+	]
+	
+	parts = [
+		"-capped",
+		"-beaked",
+		"-cheeked",
+		"-breasted",
+		"-footed",
+		"-eyed",
+		"-tongued",
+		"-tailed",
+		"-winged",
+		"-crested",
+		"-ringed",
+		"-striped",
+		"-spotted"
+	]
+		
+	species = [
+		"Hawk",
+		"Eagle",
+		"Falcon",
+		"Jay",
+		"Junco",
+		"Finch",
+		"Osprey",
+		"Swallow",
+		"Duck",
+		"Swan",
+		"Goose",
+		"Chickadee",
+		"Sparrow",
+		"Conure",
+		"Dove",
+		"Ibis",
+		"Owl",
+		"Gull"
+	]
+	
+	names = [
+		"Jane",
+		"Marie",
+		"Nova",
+		"Hal",
+		"Ash",
+		"Hayley",
+		"Harrison Ford",
+		"Disney",
+		"Katie",
+		"Mario & Luigi",
+		"Michelle"
+	]
+
+	def makeBird(colors, parts, species):
+		for i in range(1,10):
+			print("- " +(random.choice(colors)) + "" + (random.choice(parts)) + " " + (random.choice(species)))
+			
+	def nameBird(names, colors, species):
+		for i in range(1,10):
+			print("- " +(random.choice(names)) + "'s " + (random.choice(colors)) + " " + (random.choice(species)))
+				
+	#selection = int(return input())	
+
+	#print("Hit ENTER to generate NEW BIRDS!!")
+	print("CHECK OUT THESE SWEET NEW BIRDS!!!")
+	makeBird(colors, parts, species)
+	print(" ")
+	print("CHECK OUT THESE OTHER AND EQUALLY SWEET NEW BIRDS!!!")
+	nameBird(names, colors, species)
+	
+main()
